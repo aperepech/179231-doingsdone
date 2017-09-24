e9e02ac39c36732456d3378e139b2a895fff9d55
Andrei Perepech <aperepech@yandex.ru>
2017-09-24 21:58:32 +0300

    исправ

--- functions.php
+++ functions.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * Read data from DB and return data array
- * @param (resource) $con
- * @param (string) $sql
- * @param (array) $data
- * @return array
- */
-
 function select_data($con, string $sql, array $data = [])
 {
     $stmt =  db_get_prepare_stmt($con, $sql, $data);
@@ -22,44 +14,27 @@ function select_data($con, string $sql, array $data = [])
     return [];
 }
 
-/*
- * Insert data to DB
- * @param (resource) $con
- * @param (string) $table
- * @param (array) $data
- * @return false or int
- */
-
 function insert_data($con, string $table, array $data)
 {
-    $keys = [];
-    $values = [];
-
-    foreach ($data as $key => $value) {
-        array_push($keys, $key);
-        array_push($values, "'".$value."'");
+	$keys = array_keys($data);
+    $cols = implode(', ', $keys);
+    $values = array_values($data);
+    $vals = str_repeat('?, ', count($values));
+    $vals = substr($vals, 0, -2);
+ 
+    $sql = "INSERT INTO $table ($cols) VALUES ($vals)";
+ 
+    $stmt = db_get_prepare_stmt($con, $sql, $values);
+	
+    if (mysqli_stmt_execute($stmt)) {
+         return mysqli_insert_id($con);
     }
-
-    $sql = 'INSERT INTO '.$table.' ('.implode(", ", $keys).') VALUES ('.implode(', ', $values).');';
-    $stmt = db_get_prepare_stmt($con, $sql);
-
-    if($stmt) {
-        mysqli_stmt_execute($stmt);
-        return mysqli_insert_id($con);
-    }
-
+    
     return false;
+ 
 }
 
-/*
- * Run SQL-query (for example, update or delete)
- * @param (resource) $con
- * @param (string) $sql
- * @param (array) $data
- * @return boolean
- */
-
-function exec_query ($con, string $sql, array $data = []) {
+function exec_query($con, string $sql, array $data = []) {
 
     return mysqli_stmt_execute(db_get_prepare_stmt($con, $sql, $data));
 
@@ -82,18 +57,18 @@ function renderTemplate($dir, $data)
 }
 
 
-function countTasksByCategory($category, $tasks)
+function countTasksByCategory($project_id, $tasks)
 {
     $count = 0;
-    if ($category === "Все")
+    if ($project_id === 0)
     {
         $count = count($tasks);
     }
     else
     {
-        for($i=0; $i<count($tasks); $i++)
+        foreach($tasks as $t)
         {
-            if ($tasks[$i]['category'] === $category)
+            if ($t['project_id'] === $project_id)
             {
                 $count++;
             }
@@ -103,8 +78,26 @@ function countTasksByCategory($category, $tasks)
 }
 
 function isValidDate(string $strDate)
- {
+{
     return preg_match('/^\d{2}\.\d{2}\.\d{4}$/', $strDate);
- }
+}
+
+function dateFromBase($date)
+{
+    if ($date=='' || $date==null)
+        return '';
+
+    return \DateTime::createFromFormat('Y-m-d H:i:s', $date)->format('d.m.Y');
+}
+
+function dateToBase($date)
+{
+    return \DateTime::createFromFormat('d.m.Y',$date)->format('Y-m-d');
+}
+
+function checkTaskId($con, $task_id, $user_id)
+{
+    return count(select_data($con, 'SELECT tasks.id FROM tasks, projects WHERE tasks.id= ? AND date_done IS NULL AND tasks.project_id=projects.id AND projects.user_id = ?',[$task_id, $user_id]))==1;
+}
 
  
